1f5546b8e6eb3e13df367dc33cc594b0d2fc0081
Malhadi Jr <malhadijr@icloud.com>
2019-07-14 17:00:21 +0800

    membuat fitur onclick dengan identifier className

--- dom.js
+++ dom.js
@@ -1,3 +1,10 @@
 function rubahWarnaJudul() {
 	document.getElementById("judul").style.color="red"
 }
+
+function rubahWarnaNama() {
+	var elements = document.getElementsByClassName("nama")
+	for (var i = 0; i < elements.length; i++) {
+		elements[i].style.color="green"
+	}
+}
